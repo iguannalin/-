e9a654a523f0bb62082b51c959038e75702dbfb8
seasons986 <annalin12345@gmail.com>
2025-02-06 10:23:31 -0500

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", () => {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+  const container = document.getElementById("container");
+  let characters = [];
+
+  // data from https://www.tutormandarin.net/en/list-of-different-simplified-and-traditional-characters/
+  fetch("characters.json")
+    .then((response) => response.json())
+    .then(data => {
+      characters = data;
+      generate();
+    });
+
+  function display(ch) {
+    container.innerHTML = "";
+    console.log(ch);
+    const elem1 = document.createElement("div");
+    elem1.className = "jian";
+    elem1.innerText = ch.jian;
+    const elem2 = document.createElement("div");
+    elem2.className = "fan";
+    elem2.innerText = ch.fan;
+    container.appendChild(elem1);
+    container.appendChild(elem2);
+  }
+
+  function generate() {
+    display(characters[getRandomInt(0, characters.length)]);
+  }
+
+  document.getElementById("generate").addEventListener("click", generate);
+});
